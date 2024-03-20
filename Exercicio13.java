53f632fa245919ae350016936a36aa9e9d1059fd
GKmargo <giovanitccamargo@gmail.com>
2024-03-20 16:46:44 -0300

    Exercicio 13

--- Exercicio13.java
+++ Exercicio13.java
@@ -8,11 +8,11 @@ public class Exercicio13 {
             f[0] = 1; //declarando que o primeiro termo é 1
         }
         if (n > 1) {
-            f[1] = 1; //declarando que o segundo termo é 1
+            f[1] = 1; //declarando que o segundo termo também é 1
         }
 
         for (int i = 2; i < n; i++) {
-            f[i] = f[i-1] + f[i-2]; // cada termo que vem depois é a soma dos dois termos anteriores
+            f[i] = f[i-1] + f[i-2]; //cada termo que vem depois é a soma dos dois termos anteriores
         }
 
         Prompt.imprimir("Os " + n + " primeiros termos da sequência de Fibonacci:");
