9e318f98f215a4836aa43f57d3a729fc0de75875
GKmargo <giovanitccamargo@gmail.com>
2024-03-20 10:11:02 -0300

    Exercicio 02

--- Exercicio02.java
+++ Exercicio02.java
@@ -5,14 +5,14 @@ public class Exercicio02 {
         double[] num = new double[qtd];
 
          for (int i = 0; i < qtd; i++){
-            num[i] = Prompt.lerDecimal("Digite o número da posição " + (i + 1) + ":");
+            num[i] = Prompt.lerDecimal("Digite o " + (i + 1) + " número:");
         }
         
         for (int i = 0; i < qtd; i ++){
             if (num[i] > 0){ 
-                Prompt.imprimir("Número: " + (i + 1) + " é positivo");
+                Prompt.imprimir("Número: " + (num[i]) + " é positivo");
             } else if (num[i] < 0){
-                Prompt.imprimir("Número: " + (i + 1) + " é negativo");
+                Prompt.imprimir("Número: " + (num[i]) + " é negativo");
             } else if (num[i] == 0){
                 Prompt.imprimir("Número: " + (i + 1) + " é Zero");
             }
