0377e6cf423045ba5da99bcecafdf02b0ea43066
GKmargo <giovanitccamargo@gmail.com>
2024-03-20 10:24:27 -0300

    Exercicio 03

--- Exercicio03.java
+++ Exercicio03.java
@@ -1,5 +1,17 @@
 public class Exercicio03{
     public static void executar(){
-        
+        int qtd = Prompt.lerInteiro("Digite a quantidade:");
+
+        double[] num = new double[qtd];
+        double[] dobro = new double[qtd];
+
+         for (int i = 0; i < qtd; i++){
+            num[i] = Prompt.lerDecimal("Digite o " + (i + 1) + " número:");
+            dobro[i] = num[i] * 2;
+        }
+
+        for (int i = 0; i < qtd; i++){
+            Prompt.imprimir("O dobro do número: " + (i+1) + " : " + dobro[i]);
+        }
     }
 }
