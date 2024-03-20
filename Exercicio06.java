c9ef9a1b0927fed01b1f340e49bcfd0334f78fa8
GKmargo <giovanitccamargo@gmail.com>
2024-03-20 14:30:10 -0300

    Exercicio 06

--- Exercicio06.java
+++ Exercicio06.java
@@ -3,5 +3,19 @@ public class Exercicio06{
         double[] nota = new double[5];
         double[] peso = new double[5];
 
+        double somaPeso = 0;
+        double media = 0;
+
+        for (int i = 0; i < 5; i++){
+            nota[i] = Prompt.lerDecimal("Digite a nota " + (i+1) + " do aluno:");
+            peso[i] = Prompt.lerDecimal("Digite o peso da nota " + (i+1) + ":");
+            
+            somaPeso += peso[i];
+            media += nota[i] * peso[i];
+        }
+
+        media /= somaPeso;
+        Prompt.imprimir("A media ponderada do aluno é: " + media);
+
     }
 }
