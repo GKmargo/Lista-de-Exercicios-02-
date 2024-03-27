37b642b19590c10ab44c09276e240dc158f91728
Auto Logon <autologon@CSED.LAB>
2024-03-27 09:10:45 -0300

    Exertcicio 15 - atualizado

--- Exercicio15.java
+++ Exercicio15.java
@@ -3,29 +3,22 @@ public class Exercicio15 {
         int[] vetor = new int[5];
         int ultimoValor = Integer.MIN_VALUE; //retorna o menor valor possivel 
 
-        // Loop para ler os elementos do vetor
+        
         for(int i = 0; i < vetor.length; i++){
             int valorAtual;
-
-            // Se for o primeiro elemento, aceita qualquer valor
             if(i == 0){
                 valorAtual = Prompt.lerInteiro("Digite o valor " + (i + 1) + " do vetor:");
             } else {
-                // Para os demais elementos, só aceita valores maiores que o anterior
                 do {
                     valorAtual = Prompt.lerInteiro("Digite um valor maior que " + ultimoValor + ":");
                 } while(valorAtual <= ultimoValor);
             }
-
             vetor[i] = valorAtual;
             ultimoValor = valorAtual;
         }
-
-        // Imprime o vetor
         Prompt.imprimir("Vetor:");
         for(int valor : vetor){
             Prompt.imprimir(valor);
         }
     }
-
 }
